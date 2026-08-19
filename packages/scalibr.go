2a8f3d0f4f8f9584376fd9d1798f51cb0e15f109
Dmitrii Ganochenko <dmitriig@google.com>
2026-08-19 12:25:17 +0200

    extractors logix fix

--- packages/scalibr.go
+++ packages/scalibr.go
@@ -154,32 +154,6 @@ func (p scalibrInstalledPackagesProvider) GetInstalledPackages(ctx context.Conte
 
 	pkgs := pkgInfosFromExtractorPackages(ctx, scan, &osinfo)
 
-	// Hybrid fallback: if base OS extractors are not included in SCALIBR allowed extractors, use native extractors.
-	hasExtractor := func(name string) bool {
-		for _, e := range p.extractors {
-			if e == name {
-				return true
-			}
-		}
-		return false
-	}
-
-	if !hasExtractor("os/dpkg") && DpkgQueryExists && len(pkgs.Deb) == 0 {
-		if deb, err := InstalledDebPackages(ctx); err == nil {
-			pkgs.Deb = enrichDebPkgInfoWithPurl(deb, osinfo.ShortName, osinfo.Version)
-		}
-	}
-	if !hasExtractor("os/rpm") && RPMQueryExists && len(pkgs.Rpm) == 0 {
-		if rpm, err := InstalledRPMPackages(ctx); err == nil {
-			pkgs.Rpm = enrichRpmPkgInfoWithPurl(rpm, osinfo.ShortName, osinfo.Version)
-		}
-	}
-	if !hasExtractor("os/cos") && COSPkgInfoExists && len(pkgs.COS) == 0 {
-		if cos, err := InstalledCOSPackages(); err == nil {
-			pkgs.COS = enrichCosPkgInfoWithPurl(cos, osinfo.ShortName, osinfo.Version)
-		}
-	}
-
 	// TODO: replace zypper patches legacy extractor with implemented "os/zypper" extractor
 	if ZypperExists {
 		zypperPatches, err := ZypperInstalledPatches(ctx)
